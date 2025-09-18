d2afb56bef65cba0f7748bb9078d0c2a1210ffce
shriharshan-param <shriharshan@param.network>
2025-09-18 12:02:24 +0530

    Added Server Config URL in config.py

--- config.py
+++ config.py
@@ -10,3 +10,4 @@ class Config:
     TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
     TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
     TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
+    SERVER_BASE_URL = os.getenv("SERVER_BASE_URL")
